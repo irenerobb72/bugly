be9637db437fcd4e086dca7271ea55f8432246e9
Irene Robb <irenerobb72@gmail.com>
2016-03-20 22:55:03 -0400

    fixed it so it is random instead of up

--- app/index.js
+++ app/index.js
@@ -1,30 +1,31 @@
-var num = 50000
+var num = 20000
+
 var canvas = document.getElementById("myCanvas")
-var width = canvas.width = 2000
-var height = canvas.height = 1000
+var width = canvas.width = 1500
+var height = canvas.height = 800
 var ctx = canvas.getContext("2d")
 
 var particles = d3.range(num).map(function(i) {
   return [Math.round(width*Math.random()), Math.round(height*Math.random())]
 })
 
-d3.timer(init)
+d3.timer(step)
 
-function init() {
-  ctx.fillStyle = "rgba(0,0,0,0.3)"
+function step() {
+  ctx.fillStyle = "rgba(255,255,255,0.3)"
   ctx.fillRect(0,0,width,height)
-  ctx.fillStyle = "rgba(250,250,250,0.5)"
-  particles.map(function(p) {
+  ctx.fillStyle = "rgba(0,0,0,0.5)"
+  particles.forEach(function(p) {
     p[0] += Math.round(2*Math.random()-1)
-    p[1] += Math.floor(2*Math.random()-1)
+    p[1] += Math.round(2*Math.random()-1)
     if (p[0] < 0) p[0] = width
     if (p[0] > width) p[0] = 0
     if (p[1] < 0) p[1] = height
     if (p[1] > height) p[1] = 0
     drawPoint(p)
- })
+  })
 }
 
 function drawPoint(p) {
   ctx.fillRect(p[0],p[1],1,1)
-}
+};
